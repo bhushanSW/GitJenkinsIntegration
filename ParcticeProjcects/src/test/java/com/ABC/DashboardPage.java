0eb446c47405ad2fea9f06b7107b11a1f124a929
bhushanSW <bhushanwaghbw98@gmail.com>
2022-04-14 13:50:02 +0530

    Adding newer project

---
+++ ParcticeProjcects/src/test/java/com/ABC/DashboardPage.java
@@ -0,0 +1,41 @@
+package com.ABC;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DashboardPage {
+    @Test
+    public void checkmsg01() {
+    	System.setProperty("webdriver.chrome.driver", "chromedriverNew.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(
+				"file:///C:/Users/Admin/Downloads/B.selenium/javabykiran-Selenium-Softwares/Offline%20Website/index.html");
+		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[1]/input")).sendKeys("kiran@gmail.");
+		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/input")).sendKeys("741852");
+		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[3]/div/button")).click();
+		String Msg=driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div")).getText();
+		driver.close();
+		Assert.assertEquals(Msg,"Please enter password 123456");
+		
+
+    }
+    @Test
+    public void checkmsg02() {
+    	System.setProperty("webdriver.chrome.driver", "chromedriverNew.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(
+				"file:///C:/Users/Admin/Downloads/B.selenium/javabykiran-Selenium-Softwares/Offline%20Website/index.html");
+		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[1]/input")).sendKeys("kiran@gmail123456");
+		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/input")).sendKeys("123456");
+		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[3]/div/button")).click();
+		String Msg=driver.findElement(By.xpath("/html/body/div/div[2]/form/div[1]/div")).getText();
+		driver.close();
+		Assert.assertEquals(Msg,"Please enter email as kiran@gmail.com");
+		
+	
+    	
+    }
+}
